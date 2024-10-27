bc382afa59e5596caf4e9c8246613c0de7ae235a
hyChia88 <hychia2024@gmail.com>
2024-10-27 00:04:24 -0400

    main

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,14 @@
-text = "hello, git!"
-print(text)
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
